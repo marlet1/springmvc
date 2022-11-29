a1eee9699033088f8df9e76a39bf02711ab27fec
marlet1 <fktm2356@gmail.com>
2022-11-29 14:24:51 +0900

    s3

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -1,2 +1,33 @@
-package hello.springmvc.basic.requestmapping;public class MappingClassController {
+package hello.springmvc.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+public class MappingClassController {
+    /**
+     * 회원 목록 조회:GET '/users'
+     * 회원 등록: POST   '/users'
+     * 회원 조회: GET    '/users/{userId}'
+     * 회원 수정: PATCH  '/users/{userId}'
+     * 회원 삭제: DELETE '/users/{userId}'
+     */
+    @GetMapping("/mapping/users")
+    public String user(){
+        return "get users";
+    }
+    @PostMapping("/mapping/users")
+    public String addUser(){
+        return "post user";
+    }
+    @GetMapping("/mapping/users/{userId}")
+    public String findUser(@PathVariable String usserId){
+        return "get userId="+usserId;
+    }
+    @PatchMapping("/mapping/users/{userId}")
+    public String updateUser(@PathVariable String userId){
+        return "update userId="+userId;
+    }
+    @DeleteMapping("/mapping/users/{userId}")
+    public String deleteUser(@PathVariable String userId){
+        return "delete userId="+userId;
+    }
 }
